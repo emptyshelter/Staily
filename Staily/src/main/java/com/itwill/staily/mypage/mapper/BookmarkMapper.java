1e9f256fe0d7f7e98643543acbc329d8daa123d7
hiphopmy@naver.com <hiphopmy@naver.com>
2020-03-24 15:56:02 +0900

    commit

--- Staily/src/main/java/com/itwill/staily/mypage/mapper/BookmarkMapper.java
+++ Staily/src/main/java/com/itwill/staily/mypage/mapper/BookmarkMapper.java
@@ -1,5 +1,15 @@
 package com.itwill.staily.mypage.mapper;
 
-public interface BookmarkMapper {
+import java.util.List;
 
+import com.itwill.staily.mypage.model.dto.Bookmark;
+
+public interface BookmarkMapper {
+	public List<Bookmark> bookmarkList();
+	
+	public int insertBookmark();
+	
+	public int deleteBookmark();
+	
+	
 }
