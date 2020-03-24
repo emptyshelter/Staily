7c24b4cb3e159f2981297ea1c91ae632b27242bb
gamecover4566 <hanju45666@gmail.com>
2020-03-24 19:54:15 +0900

    mapper 작성

--- Staily/src/main/java/com/itwill/staily/detail/mapper/CommentMapper.java
+++ Staily/src/main/java/com/itwill/staily/detail/mapper/CommentMapper.java
@@ -1,5 +1,15 @@
 package com.itwill.staily.detail.mapper;
 
-public interface CommentMapper {
+import java.util.List;
 
+import com.itwill.staily.detail.model.dto.Comment;
+
+public interface CommentMapper {
+	public List<Comment> selectCommentList(int pNo);
+	
+	public boolean createComment(Comment comment);
+	
+	public boolean deleteComment(Comment comment);
+	
+	public boolean checkCommentUser(Comment comment, int mNo);
 }
--- Staily/src/main/java/com/itwill/staily/detail/mapper/WorkdetailMapper.java
+++ Staily/src/main/java/com/itwill/staily/detail/mapper/WorkdetailMapper.java
@@ -1,5 +1,7 @@
 package com.itwill.staily.detail.mapper;
 
-public interface WorkdetailMapper {
+import com.itwill.staily.util.Work;
 
+public interface WorkdetailMapper {
+	public Work selectWorkOne(int wNo);
 }
