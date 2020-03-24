05274bd330e1b55b44cbc03a2fb1a6c2d8a68cf9
ujandgd@naver.com <ujandgd@naver.com>
2020-03-24 19:38:25 +0900

    .

---
+++ Staily/src/main/java/com/itwill/staily/main/model/dao/MainDao.java
@@ -0,0 +1,11 @@
+package com.itwill.staily.main.model.dao;
+
+import java.util.List;
+
+import com.itwill.staily.util.Product;
+import com.itwill.staily.util.Work;
+
+public interface MainDao{
+	
+
+}
---
+++ Staily/src/main/java/com/itwill/staily/main/model/dao/MainDaoImpl.java
@@ -0,0 +1,5 @@
+package com.itwill.staily.main.model.dao;
+
+public class MainDaoImpl{
+	
+}
