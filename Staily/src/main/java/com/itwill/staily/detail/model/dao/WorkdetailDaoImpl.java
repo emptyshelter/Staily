9ed9f17d77a2790d861c582f2b709e0cd1c68864
gamecover4566 <hanju45666@gmail.com>
2020-03-24 20:13:49 +0900

    dao, mapper 작성

---
+++ Staily/src/main/java/com/itwill/staily/detail/model/dao/WorkdetailDaoImpl.java
@@ -0,0 +1,13 @@
+package com.itwill.staily.detail.model.dao;
+
+import com.itwill.staily.util.Work;
+
+public class WorkdetailDaoImpl implements WorkdetailDao {
+
+	@Override
+	public Work selectWorkOne(int wNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
