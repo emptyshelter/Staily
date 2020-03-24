091abdcf30272062b75b332aab0697842ee6513f
hiphopmy@naver.com <hiphopmy@naver.com>
2020-03-24 16:57:28 +0900

    commit!

--- Staily/src/main/java/com/itwill/staily/mypage/mapper/MessageMapper.java
+++ Staily/src/main/java/com/itwill/staily/mypage/mapper/MessageMapper.java
@@ -1,5 +1,17 @@
 package com.itwill.staily.mypage.mapper;
 
-public interface MessageMapper {
+import java.util.List;
 
+import com.itwill.staily.mypage.model.dto.Message;
+
+public interface MessageMapper {
+	public Message messageSelect();
+	
+	public List<Message> messageList();
+	
+	public int insertMessage();
+	
+	public int deleteMessage();
+	
+	public int updateMessage();
 }
